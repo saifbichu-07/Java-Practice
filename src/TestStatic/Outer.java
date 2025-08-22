55b3be52cf218aeacde463d9f70b91f312d51c5a
saifbichu- <bichusaif@gmail.com>
2025-08-22 12:08:49 +0530

    static class

---
+++ src/TestStatic/Outer.java
@@ -0,0 +1,11 @@
+package TestStatic;
+
+public class Outer {
+    public static class Inner {
+        public static int x = 5;
+        public static void message() {
+            System.out.println("Hello static");
+        }
+
+    }
+}
