f23f3373634ae382d1ee4a06a92f5a9d027601b7
saifbichu- <bichusaif@gmail.com>
2025-08-30 21:34:19 +0530

    Exception Handling

--- src/main.java
+++ src/main.java
@@ -1,16 +1,29 @@
-public class main {
-    public static void main(String[] args) {
-        // Creating a Constructor object using the default constructor
-        Constructor parkedConstructor = new Constructor();
-        // Output will indicate that the Constructor is parked
-        parkedConstructor.startConstructor();
 
-        System.out.println("---------------------");
 
-        // Creating a Constructor object using the parameterized constructor
-        // The parameters simulate a running Constructor scenario: closed door, engine on, driver seated, and a speed of 10 km/h
-        Constructor runningConstructor = new Constructor("closed", "on", "seated", 10);
-        // Output will show the running Constructor state
-        runningConstructor.startConstructor();
+public class Main {
+    public static void main(String[]args){
+     Main main = new Main();
+     try{
+         main.doSomething();
+     }catch (ZeroException e) {
+
+         e.printStackTrace();
+     }
+
+
     }
+    class ZeroException extends Exception{
+
+    }
+    public void doSomething() throws ZeroException{
+        String x = "zero";
+        if(x.equals("zero")){
+            throw new ZeroException();
+        }
+        System.out.println("Do something");
+
+
+    }
+
+
 }
